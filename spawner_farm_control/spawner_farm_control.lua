7ff3484524c2f791b3d03f0575d469599ff8f40f
Geoffroy Panis <jojopanis@gmail.com>
2025-10-23 20:05:36 +0200

    .

--- spawner_farm_control/spawner_farm_control.lua
+++ spawner_farm_control/spawner_farm_control.lua
@@ -1,7 +1,7 @@
 local screen = peripheral.find('monitor')
 term.redirect(screen)
 
---Variable Definition
+--Variables Definition
 local spawner_list = {
     {label = 'cav_creeper',relay_id = 54},
     {label = 'witch',relay_id = 73},
@@ -12,17 +12,8 @@ local menu_list = {
 local menu_map = {}
 local button_map = {}
 
---Function Definition
-local function draw_button(x,y,color,message,sizeX,sizeY)
-    sizeX = sizeX or 7
-    sizeY = sizeY or 2
-    message = message or "......"
-    color = color or colors.red
-    paintutils.drawFilledBox(x,y,x+sizeX,y+sizeY,color)
-    term.setCursorPos(x+1,y+1)
-    term.setTextColor(colors.black)
-    write(string.sub(message,1,6))
-end
+--Functions Definition
+--Drawing Functions
 local function draw_button2(button,item,is_menu)
     local x = button.x
     local y = button.y
@@ -46,7 +37,6 @@ end
 local function init_buttons(map,list,is_menu)
     color = color or colors.cyan
     for i,item in ipairs(list) do
-        --draw_button(map[i].x, map[i].y, color, item.label)
         if map[i].state == nil then
             map[i].state = false
         end
@@ -54,15 +44,13 @@ local function init_buttons(map,list,is_menu)
     end
 end
 
-local function draw_menu()
+local function draw_spawner_screen()
+    init_buttons(button_map,spawner_list,false)
     paintutils.drawLine(61,1,61,26,colors.gray)
     init_buttons(menu_map,menu_list,true)
 end
 
-local function draw_spawners()
-    init_buttons(button_map,spawner_list,false)
-end
-
+--Logic Functions
 local function button_clicked(x,y,map)
     for i,button in ipairs(map) do
         if x >= button.x and x <= button.x+7 and y >= button.y and y <= button.y+2 then
@@ -71,27 +59,32 @@ local function button_clicked(x,y,map)
     end
 end
 
-
-for i=2,52,10 do
-    for j=2,22,4 do
-        table.insert(button_map,{x=i,y=j,state=nil})
+local function populate_buttons()
+    for i=2,52,10 do
+        for j=2,22,4 do
+            table.insert(button_map,{x=i,y=j,state=nil})
+        end
     end
 end
 
-for i= 2,22,4 do
-    table.insert(menu_map, {x=63, y=i})
+local function populate_menu()
+    for i= 2,22,4 do
+        table.insert(menu_map, {x=63, y=i})
+    end
 end
 
 term.setBackgroundColor(colors.black)
 term.clear()
 term.setTextColor(colors.white)
+populate_buttons()
+populate_menu()
 
 while true do
-    draw_spawners()
+    draw_spawner_screen()
     draw_menu()
     local _,_,x,y = os.pullEvent("monitor_touch")
-    local id = button_clicked(x,y,button_map)
-    if id ~= nil then
-        button_map[id].state = not button_map[id].state
+    local s_id = button_clicked(x,y,button_map)
+    if s_id ~= nil then
+        button_map[s_id].state = not button_map[s_id].state
     end
 end
